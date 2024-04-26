6db7e2aedcd4e2f660eea7b02720a745e155148a
PanBard <93971955+PanBard@users.noreply.github.com>
2024-04-26 18:53:42 +0200

    Add files via upload

---
+++ frontend/src/lib/styles/theme.ts
@@ -0,0 +1,16 @@
+import { AppTheme } from "../types";
+
+export const myTheme: AppTheme = {
+       colors: {
+        primary: '#3498db',
+        background: '#282728',
+        foreground: '#30363d',
+        error: '#c44569',
+        typography: '#c8d6e5',
+        input: '#484748',
+        button_hover: '#3b3a3b'
+    },
+    media: {
+        sm: 900
+    }
+}
